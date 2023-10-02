3b7d08f9c8c87e188d73d6f3b933917bd8110752
DL <dl@thespr3.com>
2023-10-02 00:17:49 -0700

    path fix

--- generator.js
+++ generator.js
@@ -1,4 +1,4 @@
-import { generate } from "../generator/index.js";
+import { generate } from "./generator/index.js";
 
 self.onmessage = ({ data: settings }) => {
   const result = generate(settings);
